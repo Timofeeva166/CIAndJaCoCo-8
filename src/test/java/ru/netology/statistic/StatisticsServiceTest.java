9a931a6cf6309e307f8761ba46e9332a9450bbf7
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-06-28 15:29:50 +0300

    Tests added

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Assertions;
 public class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMaxIfFirstMax() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfFirstNotMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {3, 7, 9, 4, 5, 1, 9, 6, 15, 2, 1};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
